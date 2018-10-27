ad01c763b4275dbbfb8c36b7e68f093468ef2a6a
cwahlmann <33593291+cwahlmann@users.noreply.github.com>
2018-10-28 01:31:37 +0200

    Update SeriesDataService.java

--- flightseries/flightseries-backend/src/main/java/de/generationcode/flightseries/backend/service/SeriesDataService.java
+++ flightseries/flightseries-backend/src/main/java/de/generationcode/flightseries/backend/service/SeriesDataService.java
@@ -33,7 +33,7 @@ public class SeriesDataService {
 
 	public static void main(String[] args) throws Exception {
 		SeriesDataService service = new SeriesDataService();
-		service.jwtToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NDA3Mjc4MDgsImlkIjoiRmxpZ2h0IFNlcmllcyIsIm9yaWdfaWF0IjoxNTQwNjQxNDA4LCJ1c2VyaWQiOjUxMzYzNSwidXNlcm5hbWUiOiJtYXJ2aW4ucHVldGhlb3gzIn0.S6RvZPkoHHSzHBFsH1uLKkmIpN5ds-K9MsIeXeBRAHWs0RCvr_Zu8ryiOfU_4YIbAgtdQWyBvmfkzWMm9KS1DnKVsbFY8ePms_DcIZ6HYDJ3SbuUmYmRkXu-dZUOdrXEJjP5WPCGl4TDV9dl3JH9t2ZHZ2vBH0q0yVSdJp8m6M8867ieWNtK9od6Eka-jCEzZZ14urvJTc8_yjR4NEpkic1C4lx0Zil8cll7ek8tsh1fUmVPcVmNUwiKzlp6WuLlEXOhQqtKLgPcGKDUtA_90MQmxXURH3TXp90SX05rrUBEMRCpSIwSkP_bQHUvObPz-RZF_9bjRyKpTk-mlLq8HA";
+		service.jwtToken = "...";
 		String response = service.findSeries("supernatural");
 		System.out.println(response);
 	}
